ff2a2cb3e1828f02e6ce310727abb5ec941e53a0
Kentaro Takiguchi <takiguchi0817@gmail.com>
2013-11-04 01:12:26 +0900

    Introduce AsyncRssClient

--- Rebuild/src/main/java/rejasupotaro/rebuild/fragments/EpisodeListFragment.java
+++ Rebuild/src/main/java/rejasupotaro/rebuild/fragments/EpisodeListFragment.java
@@ -2,21 +2,31 @@ package rejasupotaro.rebuild.fragments;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ListView;
 
 import com.google.inject.Inject;
 
+import org.apache.http.Header;
+
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
+import rejasupotaro.asyncrssclient.AsyncRssClient;
+import rejasupotaro.asyncrssclient.AsyncRssResponseHandler;
+import rejasupotaro.asyncrssclient.models.RssFeed;
 import rejasupotaro.rebuild.R;
 import rejasupotaro.rebuild.adapters.EpisodeListAdapter;
 import rejasupotaro.rebuild.models.Episode;
+import rejasupotaro.rebuild.utils.ToastUtils;
 import roboguice.fragment.RoboListFragment;
 
 public class EpisodeListFragment extends RoboListFragment {
+
+    private static final String TAG = EpisodeListFragment.class.getSimpleName();
 
     @Inject
     private LayoutInflater mLayoutInflater;
@@ -40,6 +50,33 @@ public class EpisodeListFragment extends RoboListFragment {
         getListView().setFadingEdgeLength(0);
         getListView().addHeaderView(mLayoutInflater.inflate(R.layout.header_episode_list, null));
 
+        AsyncRssClient client = new AsyncRssClient();
+        client.read(
+                "http://feeds.rebuild.fm/rebuildfm",
+                new AsyncRssResponseHandler() {
+            @Override
+            public void onSuccess(RssFeed rssFeed) {
+                setupEpisodeListView();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] body, Throwable throwable) {
+                ToastUtils.show(getActivity(), "An error occurred while requesting rss feed.");
+                Log.e(TAG, "statusCode: " + statusCode);
+                for (Header header : headers) {
+                    Log.e(TAG, header.getName() + " => " + header.getValue());
+                }
+                try {
+                    Log.e(TAG, new String(body, "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                Log.e(TAG, throwable.toString());
+            }
+        });
+    }
+
+    public void setupEpisodeListView() {
         List<Episode> episodeList = new ArrayList<Episode>();
 
         episodeList.add(Episode.newDummyInstance());
---
+++ Rebuild/src/main/java/rejasupotaro/rebuild/utils/ToastUtils.java
@@ -0,0 +1,17 @@
+package rejasupotaro.rebuild.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public final class ToastUtils {
+
+    private ToastUtils() {}
+
+    public static void show(Context context, int resId) {
+        show(context, context.getString(resId));
+    }
+
+    public static void show(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
