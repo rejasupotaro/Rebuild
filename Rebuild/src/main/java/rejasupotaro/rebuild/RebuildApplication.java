f85d2321bbe26d015cc239202ccf1f172ec85afc
Kentaro Takiguchi <takiguchi0817@gmail.com>
2013-11-04 16:36:00 +0900

    Introduce ActiveAndroid

---
+++ Rebuild/src/main/java/rejasupotaro/rebuild/RebuildApplication.java
@@ -0,0 +1,19 @@
+package rejasupotaro.rebuild;
+
+import android.app.Application;
+
+import com.activeandroid.ActiveAndroid;
+
+public class RebuildApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ActiveAndroid.initialize(this);
+    }
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        ActiveAndroid.dispose();
+    }
+}
