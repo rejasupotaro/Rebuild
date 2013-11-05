bd8d1b8beb1720237afa16843222db571f612d14
Kentaro Takiguchi <takiguchi0817@gmail.com>
2013-11-05 23:26:02 +0900

    Set view visibility gone on animation ended

--- Rebuild/src/main/java/rejasupotaro/rebuild/utils/UiAnimations.java
+++ Rebuild/src/main/java/rejasupotaro/rebuild/utils/UiAnimations.java
@@ -1,19 +1,38 @@
 package rejasupotaro.rebuild.utils;
 
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.view.View;
+import android.view.animation.Animation;
 
 public final class UiAnimations {
 
     private UiAnimations() {
     }
 
-    public static void fadeOut(View view, long delay, long duration) {
+    public static void fadeOut(final View view, long delay, long duration) {
         view.setAlpha(1);
         ObjectAnimator animation = ObjectAnimator.ofFloat(view, "alpha", 0);
         animation.setStartDelay(delay);
         animation.setDuration(duration);
-        animation.start();
+        animation.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+            }
 
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                view.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+            }
+        });
+        animation.start();
     }
 }
