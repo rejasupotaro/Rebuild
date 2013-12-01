976c5cffb1c51490ad0a16e3cac851f785f754ff
Kentaro Takiguchi <takiguchi0817@gmail.com>
2013-12-01 10:35:21 +0900

    Update RobotGirl version

--- Rebuild/src/instrumentTest/java/rejasupotaro/rebuild/test/models/EpisodeTest.java
+++ Rebuild/src/instrumentTest/java/rejasupotaro/rebuild/test/models/EpisodeTest.java
@@ -1,39 +1,37 @@
 package rejasupotaro.rebuild.test.models;
 
 import android.os.Bundle;
-import android.test.AndroidTestCase;
 
 import rejasupotaro.rebuild.models.Episode;
-import rejasupotaro.rebuild.serializers.UriTypeSerializer;
+import rejasupotaro.robotgirl.ActiveAndroidTestCase;
 import rejasupotaro.robotgirl.Factory;
 import rejasupotaro.robotgirl.RobotGirl;
 
-public class EpisodeTest extends AndroidTestCase {
+public class EpisodeTest extends ActiveAndroidTestCase {
 
     @Override
-    public void setUp() throws InstantiationException, IllegalAccessException {
-        RobotGirl.init(getContext(), UriTypeSerializer.class)
-                .define(new Factory("episode1", Episode.class) {
-                    @Override
-                    public Bundle set(Bundle bundle) {
-                        bundle.putInt("episode_id", 1);
-                        return bundle;
-                    }
-                })
-                .define(new Factory("episode2", Episode.class) {
-                    @Override
-                    public Bundle set(Bundle bundle) {
-                        bundle.putInt("episode_id", 1);
-                        return bundle;
-                    }
-                })
-                .define(new Factory("episode3", Episode.class) {
-                    @Override
-                    public Bundle set(Bundle bundle) {
-                        bundle.putInt("episode_id", 3);
-                        return bundle;
-                    }
-                });
+    public void setUp() throws Exception {
+        super.setUp();
+
+        RobotGirl.define(new Factory("episode1", Episode.class) {
+            @Override
+            public Bundle set(Bundle bundle) {
+                bundle.putInt("episode_id", 1);
+                return bundle;
+            }
+        }).define(new Factory("episode2", Episode.class) {
+            @Override
+            public Bundle set(Bundle bundle) {
+                bundle.putInt("episode_id", 1);
+                return bundle;
+            }
+        }).define(new Factory("episode3", Episode.class) {
+            @Override
+            public Bundle set(Bundle bundle) {
+                bundle.putInt("episode_id", 3);
+                return bundle;
+            }
+        });
     }
 
     public void testIsSameId() {
