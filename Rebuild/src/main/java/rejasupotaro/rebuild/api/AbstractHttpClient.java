d5b7574ba9cf418f9938439b91adf65a79b76581
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2014-01-29 22:51:01 +0900

    Add null check on AbstractHttpClient

--- Rebuild/src/main/java/rejasupotaro/rebuild/api/AbstractHttpClient.java
+++ Rebuild/src/main/java/rejasupotaro/rebuild/api/AbstractHttpClient.java
@@ -1,8 +1,8 @@
 package rejasupotaro.rebuild.api;
 
-import android.util.Log;
-
 import org.apache.http.Header;
+
+import android.util.Log;
 
 import java.io.UnsupportedEncodingException;
 
@@ -11,16 +11,28 @@ public abstract class AbstractHttpClient {
     protected abstract String getTag();
 
     protected void dumpError(Header[] headers, byte[] body, Throwable throwable) {
-        for (Header header : headers) {
-            Log.e(getTag(), header.getName() + " => " + header.getValue());
+        if (headers != null) {
+            for (Header header : headers) {
+                Log.e(getTag(), header.getName() + " => " + header.getValue());
+            }
+        } else {
+            Log.e(getTag(), "headers: null");
         }
 
-        try {
-            Log.e(getTag(), new String(body, "UTF-8"));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+        if (body != null) {
+            try {
+                Log.e(getTag(), new String(body, "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.e(getTag(), "body: null");
         }
 
-        Log.e(getTag(), throwable.toString());
+        if (throwable != null) {
+            Log.e(getTag(), throwable.toString());
+        } else {
+            Log.e(getTag(), "throwable: null");
+        }
     }
 }
