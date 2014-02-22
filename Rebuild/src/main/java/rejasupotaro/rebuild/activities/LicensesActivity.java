bd48c77d04264eb9f3671cf87a73207f05df1690
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2014-02-23 00:03:04 +0900

    Create LicensesActivity

---
+++ Rebuild/src/main/java/rejasupotaro/rebuild/activities/LicensesActivity.java
@@ -0,0 +1,47 @@
+package rejasupotaro.rebuild.activities;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+import javax.inject.Inject;
+
+import rejasupotaro.rebuild.R;
+import rejasupotaro.rebuild.tools.MenuDelegate;
+import roboguice.inject.InjectView;
+
+public class LicensesActivity extends RoboActionBarActivity {
+
+    private static final String LICENSES_FILE_PATH = "file:///android_asset/licenses.html";
+
+    @InjectView(R.id.licenses_view)
+    private WebView mLicensesView;
+
+    @Inject
+    private MenuDelegate mMenuDelegate;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_licenses);
+        setupActionBar();
+        setupLicensesView();
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+    }
+
+    private void setupLicensesView() {
+        mLicensesView.getSettings().setJavaScriptEnabled(false);
+        mLicensesView.loadUrl(LICENSES_FILE_PATH);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mMenuDelegate.onItemSelect(item);
+    }
+}
