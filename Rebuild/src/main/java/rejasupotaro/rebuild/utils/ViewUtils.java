4b6b118f335327d47870ebaa1cd070896defa870
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2014-03-01 15:04:21 +0900

    Linkify @mention and #hash

--- Rebuild/src/main/java/rejasupotaro/rebuild/utils/ViewUtils.java
+++ Rebuild/src/main/java/rejasupotaro/rebuild/utils/ViewUtils.java
@@ -1,23 +1,35 @@
 package rejasupotaro.rebuild.utils;
 
-import android.text.Html;
-import android.text.TextUtils;
-import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class ViewUtils {
 
     public static void setTweetText(TextView textView, String text) {
-        String tweetText = StringUtils.buildTwitterLinkText(text);
-        if (TextUtils.isEmpty(tweetText)) {
-            textView.setText(text);
-        } else {
-            textView.setMovementMethod(LinkMovementMethod.getInstance());
-            textView.setText(Html.fromHtml(tweetText));
-            textView.setFocusable(false);
-        }
+        textView.setText(text);
+
+        Linkify.TransformFilter filter = new Linkify.TransformFilter() {
+            public final String transformUrl(final Matcher match, String url) {
+                return match.group();
+            }
+        };
+
+        Pattern mentionPattern = Pattern.compile("@([A-Za-z0-9_-]+)");
+        String mentionScheme = "http://www.twitter.com/";
+        Linkify.addLinks(textView, mentionPattern, mentionScheme, null, filter);
+
+        Pattern hashtagPattern = Pattern.compile("#([A-Za-z0-9_-]+)");
+        String hashtagScheme = "http://www.twitter.com/search/";
+        Linkify.addLinks(textView, hashtagPattern, hashtagScheme, null, filter);
+
+        Pattern urlPattern = Patterns.WEB_URL;
+        Linkify.addLinks(textView, urlPattern, null, null, filter);
     }
 
     public static void addHeaderView(ListView listView, View view) {
