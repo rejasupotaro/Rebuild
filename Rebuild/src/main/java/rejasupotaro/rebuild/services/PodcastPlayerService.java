b3ec77ce23c4922ad431e01d9f12094ac5709234
Motohiro Takayama <mootoh@gmail.com>
2014-03-05 02:06:35 +0900

    Only shows a notification when the app is in background.

--- Rebuild/src/main/java/rejasupotaro/rebuild/services/PodcastPlayerService.java
+++ Rebuild/src/main/java/rejasupotaro/rebuild/services/PodcastPlayerService.java
@@ -1,7 +1,10 @@
 package rejasupotaro.rebuild.services;
 
+import android.app.Activity;
+import android.app.Application;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 
 import rejasupotaro.rebuild.events.BusProvider;
@@ -12,6 +15,49 @@ public class PodcastPlayerService extends Service {
     @Override
     public void onCreate() {
         BusProvider.getInstance().register(this);
+
+        Application app = (Application)this.getApplicationContext();
+        if (app != null) {
+            app.registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
+                int activities = 1;
+                boolean isInBackground = false;
+
+                @Override
+                public void onActivityPaused(Activity activity) {
+                    activities--;
+                }
+
+                @Override
+                public void onActivityResumed(Activity activity) {
+                    if (isInBackground) {
+                        isInBackground = false;
+                        PodcastPlayerNotification.setIsInBackground(false);
+                        PodcastPlayerNotification.cancel(activity);
+                     }
+                    activities++;
+                }
+
+                @Override
+                public void onActivityDestroyed(Activity activity) {}
+
+                @Override
+                public void onActivitySaveInstanceState(Activity activity, Bundle outState) {}
+
+                @Override
+                public void onActivityStarted(Activity activity) {}
+
+                @Override
+                public void onActivityCreated(Activity activity, Bundle savedInstanceState) {}
+
+                @Override
+                public void onActivityStopped(Activity activity) {
+                    if (activities == 0) {
+                        isInBackground = true;
+                        PodcastPlayerNotification.setIsInBackground(true);
+                    }
+                }
+            });
+        }
     }
 
     @Override
