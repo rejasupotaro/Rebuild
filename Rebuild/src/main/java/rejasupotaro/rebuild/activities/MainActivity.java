4df47571eedf450b28313ceefd221cdad3c9d86c
Kentaro Takiguchi <takiguchi0817@gmail.com>
2014-03-23 02:48:45 +0900

    Transparent MainActivity's actionbar

--- Rebuild/src/main/java/rejasupotaro/rebuild/activities/MainActivity.java
+++ Rebuild/src/main/java/rejasupotaro/rebuild/activities/MainActivity.java
@@ -2,6 +2,7 @@ package rejasupotaro.rebuild.activities;
 
 import com.squareup.otto.Subscribe;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -47,6 +48,7 @@ public class MainActivity extends RoboActionBarActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BusProvider.getInstance().register(this);
+        setupActionBar();
         startServices();
         parseIntent(getIntent());
     }
@@ -74,6 +76,12 @@ public class MainActivity extends RoboActionBarActivity
         }
 
         openEpisodeDetailActivity(episodeId);
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
     }
 
     private void startServices() {
