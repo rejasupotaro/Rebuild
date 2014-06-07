5e5dd736c87ca4905d5e1cdbd31ef4a330899314
Kentaro Takiguchi <takiguchi0817@gmail.com>
2014-06-07 11:00:45 +0900

    Introduce Logger

---
+++ Rebuild/src/staging/java/rejasupotaro/rebuild/log/Logger.java
@@ -0,0 +1,15 @@
+package rejasupotaro.rebuild.log;
+
+import android.content.Context;
+
+public class Logger {
+
+    public void initialize(Context context) {
+        // dummy method
+    }
+
+    public void flush() {
+        // dummy method
+    }
+}
+
