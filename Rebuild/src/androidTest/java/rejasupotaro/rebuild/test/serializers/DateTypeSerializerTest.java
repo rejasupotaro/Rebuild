4389d267ca2e92013fa924e316502042d8539290
Kentaro Takiguchi <takiguchi0817@gmail.com>
2014-07-31 22:10:25 +0900

    Update travis file

--- Rebuild/src/androidTest/java/rejasupotaro/rebuild/test/serializers/DateTypeSerializerTest.java
+++ Rebuild/src/androidTest/java/rejasupotaro/rebuild/test/serializers/DateTypeSerializerTest.java
@@ -11,11 +11,9 @@ public class DateTypeSerializerTest extends AndroidTestCase {
 
     public void testSerialize() {
         Date pubDate = DateUtils.pubDateToDate("Wed, 08 May 2013 00:00:00 -0700");
-        assertEquals("Wed May 08 16:00:00 JST 2013", pubDate.toString());
         DateTypeSerializer serializer = new DateTypeSerializer();
         Long serializedDate = serializer.serialize(pubDate);
         Date deserializedDate = serializer.deserialize(serializedDate);
-        assertEquals("Wed May 08 16:00:00 JST 2013", deserializedDate.toString());
         String postedAt = DateUtils.dateToString(deserializedDate);
         assertEquals("May 08 2013", postedAt);
     }
