b3ae105b187b92709bef3f82b280e557a3880e17
Kentaro Takiguchi <takiguchi0817@gmail.com>
2014-07-31 22:10:32 +0900

    Fix fade in animation

--- Rebuild/src/main/java/rejasupotaro/rebuild/utils/UiAnimations.java
+++ Rebuild/src/main/java/rejasupotaro/rebuild/utils/UiAnimations.java
@@ -73,28 +73,11 @@ public final class UiAnimations {
     }
 
     public static void fadeIn(final View view, long delay, long duration) {
+        view.setVisibility(View.VISIBLE);
         view.setAlpha(0);
         ObjectAnimator animation = ObjectAnimator.ofFloat(view, "alpha", 1);
         animation.setStartDelay(delay);
         animation.setDuration(duration);
-        animation.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                view.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-            }
-        });
         animation.start();
     }
 
